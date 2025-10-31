672424614362244cb8efe03b1a3c39da70663081
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-10-31 16:07:30 +0700

    Menambah Textfield untuk menginput alamat

--- app/src/main/java/com/example/questuserinput_020/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_020/TeksEdit.kt
@@ -77,6 +77,17 @@ fun FormDataDiri(modifier: Modifier){
                 }
             }
         }
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(width = 250.dp),
+            label = {Text(text = "Alamat Lengkap")},
+            onValueChange = {
+                textAlamat = it
+            }
+        )
+
 
     }
 }
