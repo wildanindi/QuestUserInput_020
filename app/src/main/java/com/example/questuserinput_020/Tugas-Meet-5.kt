1ec64361c489eb6bd4c371205ff6acb39d62bb93
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-02 08:01:49 +0700

    Menambah background serta membuat header untuk judul formnya

--- app/src/main/java/com/example/questuserinput_020/Tugas-Meet-5.kt
+++ app/src/main/java/com/example/questuserinput_020/Tugas-Meet-5.kt
@@ -5,6 +5,26 @@ package com.example.questuserinput_020
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,7 +32,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -36,6 +63,26 @@ fun FormPendaftaran(modifier: Modifier) {
 
         val gender: List<String> = listOf("Laki-laki", "Perempuan")
         val validasi = remember { mutableStateOf(false) }
+
+
+
+
+
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.bgform2),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = stringResource(R.string.Title),
+            color = colorResource(id = R.color.putihab),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxSize()
+                .padding(top = 70.dp, start = 80.dp),
+        )
+
 
 
     }
